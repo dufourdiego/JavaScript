0157d361e811227b23c4e7fa0adb4536550c8cf3
Diego Dufour <dufourdiego@gmail.com>
2022-08-18 19:15:34 -0300

    Corrijo declaración de variables

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
       let user = "Diego";
       let password = 123456;
-      let acceso = 0;
+      let user1, passw1, acceso = 0;
 
       // Tengo 5 oportunidades para ingresar usuario y contraseña correctas, si no se bloquea el ingreso
       // Si ingresa los datos correctos sale del bucle
 
       for(i=1; i<=5; i++){
-            let user1 = prompt(`Ingresa el nombre de usuario - Intento ${i}`);
-            let passw1 = parseInt(prompt("Ingresa la contraseña"));
+            user1 = prompt(`Ingresa el nombre de usuario - Intento ${i}`);
+            passw1 = parseInt(prompt("Ingresa la contraseña"));
             if(user1 === user){
                   if(passw1 === password){
                         alert(`Bienvenido ${user1}`);
